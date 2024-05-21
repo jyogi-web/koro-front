2f52cf8dc50cd4568bca23c0757ed67ef359659b
Yanai1005 <tatsuya871_1005@icloud.com>
2024-05-22 00:44:16 +0900

    遊び方追加

--- src/scenes/InstructionsScene.js
+++ src/scenes/InstructionsScene.js
@@ -12,6 +12,15 @@ export default class InstructionsScene extends Phaser.Scene {
     create() {
         this.add.text(400, 100, '遊び方', { fontSize: '64px', fill: '#ffffff' }).setOrigin(0.5);
 
+        this.add.text(400, 200, '操作方法', { fontSize: '32px', fill: '#ffffff' }).setOrigin(0.5);
+        this.add.text(400, 250, '矢印キーまたはWASDで移動します。', { fontSize: '24px', fill: '#ffffff' }).setOrigin(0.5);
+        this.add.text(400, 300, 'コインを10枚集めて障害物を避けましょう！', { fontSize: '24px', fill: '#ffffff' }).setOrigin(0.5);
+
+        this.add.text(400, 350, 'ゲームクリア', { fontSize: '32px', fill: '#ffffff' }).setOrigin(0.5);
+        this.add.text(400, 400, 'すべてのコインを集めて、緑色の場所に行けばゲームクリアです。', { fontSize: '24px', fill: '#ffffff' }).setOrigin(0.5);
+
+        this.add.text(400, 500, 'スタートに戻る方はESCを押してください ', { fontSize: '24px', fill: '#ffffff' }).setOrigin(0.5);
+
         this.input.keyboard.once('keydown-ESC', () => {
             this.scene.start('StartScene');
         });
